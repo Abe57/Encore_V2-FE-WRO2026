9362cd601c52c65c73ab9f8d92ba28db0caca1d8
Abe57 <76176960+Abe57@users.noreply.github.com>
2026-07-15 16:52:45 -0500

    Main algorithm

--- src/main/main.ino
+++ src/main/main.ino
@@ -4,76 +4,155 @@
 #include <ServoEasing.hpp>
 #include <Ultrasonic.h>
 
+// get EVERYTHING
 
 TaskHandle_t Sensors = NULL;
 TaskHandle_t Controls = NULL;
 
+// initialize loop objects
+
 Ultrasonic uLeft(32,33);
-Ultrasonic uCenter(2,16);
+Ultrasonic uCenter(4,16);
 Ultrasonic uRight(17,5);
 
-float left = 0;
-float center = 0;
-float right = 0;
+int left = 0;
+int center = 0;
+int right = 0;
 
+// initialize proximity sensors
+//        LEFT  CENTER  RIGHT
+// TRIG |  32  |   4  |  17  |
+// ECHO |  33  |  16  |   5  |
+
+int dMin = 20;
+int dMax = 150;
+int cw = 0;
+
+#define MAX_TURN 40
+
+float rotZ = 0;
 Adafruit_BNO055 gyro = Adafruit_BNO055(55);
+sensors_event_t event;
 
-#define M_LEFT = 26;
-#define M_RIGHT = 27;
+// initialize BNO055 rotational sensor and related
 
-ServoEasing Timonteo;
+#define BUTTON 23
+
+// start button
+
+#define PWM 25
+#define FW 26
+#define BW 27
+
+// motor control pins
+
+int servo_c = 90;
+ServoEasing Timonteo; //como te odio serbastian
+
+// initialize servo
+
+int ini_angle = 0;
+int angToMatch = 0;
+int turnOffset = 0;
+
+// variables for direction algorithm
 
 void getSensors(void * parameters){
+  gyro.getEvent(&event);
+  ini_angle = event.orientation.z;
+  // get initial robot rotation
+
   for (;;){
-    Serial.println("Getting sensor data");
-
-    sensors_event_t event;
     gyro.getEvent(&event);
+    rotZ = event.orientation.z;
 
-    float rotZ = event.orientation.z;
-    
-    left = uLeft.read(CM);
-    center = uCenter.read(CM);
-    right = uRight.read(CM);
+    Serial.print("Gyro rot Z: ");Serial.println(rotZ);
+    Serial.println("----------------------------------");
 
-    delay(100);
+    left = uLeft.read();
+    center = uCenter.read();
+    right = uRight.read();
+
+    Serial.print("Left prox: ");Serial.print(left);Serial.println("cm");
+    Serial.print("Center prox: ");Serial.print(center);Serial.println("cm");
+    Serial.print("Right prox: ");Serial.print(right);Serial.println("cm");
+
+    angToMatch = ini_angle + (turnOffset*90*cw);
+
+    delay(50);
   }
 }
 
 void control(void * parameters){
+  digitalWrite(FW,HIGH);
+  digitalWrite(PWM,HIGH);
   for (;;){
-    Serial.println("Handling direction");
-    delay(2000);
+    while (center > dMin){
+      if (cw == 0){
+        if (left > dMax){
+          cw = 1;
+        }
+        if (right > dMax){
+          cw = -1;   
+        }
+      }
+      // the totally awesome algorithm
+      Timonteo.easeTo(constrain(servo_c + rotZ - angToMatch,servo_c-MAX_TURN,servo_c+MAX_TURN));
+      delay(100);
+    }
+    turnOffset++;
+    Timonteo.easeTo(servo_c+(MAX_TURN*cw));
+    while (rotZ <= angToMatch) {
+      delay(50);
+    }
   }
 }
 
 void setup() {
   Serial.begin(115200);
 
-  gyro.begin();
+  if (!gyro.begin()) {
+    Serial.println("Could not find BNO055.");
+    while(1);
+  }
   gyro.setExtCrystalUse(true);
 
   Timonteo.attach(18, 90);
+  Timonteo.setSpeed(150);
 
-  xTaskCreatePinnedToCore(
-    getSensors,
-    "Sensor Data",
-    2048,
-    NULL,
-    2,
-    &Sensors,
-    0
-  );
+  pinMode(BUTTON, INPUT_PULLUP);
+
+  pinMode(PWM, OUTPUT);
+  pinMode(FW, OUTPUT);
+  pinMode(BW, OUTPUT);
+
+  Serial.println("Press button to start");
+  while (digitalRead(BUTTON) == HIGH)
+  {
+    delay(1);
+  }
 
   xTaskCreatePinnedToCore(
     control,
     "Car Control",
-    1024,
+    4096,
     NULL,
-    1,
+    10,
     &Controls,
     1
   );
+  // start control thread on core 1
+
+  xTaskCreatePinnedToCore(
+    getSensors,
+    "Sensor Data",
+    4096,
+    NULL,
+    1,
+    &Sensors,
+    0
+  );
+  // start sensors thread on core 0
 }
 
 void loop() {
